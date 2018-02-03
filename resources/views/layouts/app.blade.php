d23543a7c3c425c439c7e4e587307443facaee48
Gerard Reches <gerardreches@gmail.com>
2018-02-03 12:46:16 +0000

    Remove footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,22 +5,5 @@
     @include('layouts.partials.header')
     
     @yield('content')
-    
-    @include('layouts.partials.footer')
-
-    <cookies-advise href="{{ route('site.legal') }}"></cookies-advise>
-
-    <unavailable-link></unavailable-link>
-
-    <contact-dialog></contact-dialog>
-
-    <appointment-dialog></appointment-dialog>
-
-    <urgency-dialog></urgency-dialog>
-    
-    <div id="page-loader" class="d-flex justify-content-center align-items-center" hidden>
-        <img src="{{ asset('images/imagotipo-menu.svg') }}" height=120 alt="">
-        <page-loader></page-loader>
-    </div>
 
 @stop
