9a154a9c05f4b2630181b458509e430f51499fdf
josep <jssdlcu1331@gmail.com>
2018-02-03 21:13:31 +0100

    changes header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -5,22 +5,22 @@
   app
 >
  <v-list dense>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>home</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Profile</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>contact_mail</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Logout</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+    <v-list-tile @click="">
+      <v-list-tile-action>
+        <v-icon>home</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Profile</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile @click="">
+      <v-list-tile-action>
+        <v-icon>contact_mail</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Logout</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
 </v-list>
 
 </v-navigation-drawer>
