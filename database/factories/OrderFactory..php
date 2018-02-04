7bc1eb20e3a75b2eeca6daabf431851a819dc3f6
josep <jssdlcu1331@gmail.com>
2018-02-04 03:05:46 +0100

    factories

--- database/factories/OrderFactory..php
+++ database/factories/OrderFactory..php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 $factory->define(Order::class, function (Faker $faker) {
     return [
         'user_id' => 1,
-        'location_id' => $faker->numberBetween(1, 100) ,
+        'location_id' => $faker->numberBetween(1, 100),
         'title' => $faker->word,
         'description' => $faker->paragraph(1),
         'email' => $faker->unique()->safeEmail,
